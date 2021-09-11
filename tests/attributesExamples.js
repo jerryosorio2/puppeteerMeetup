a0a2e79a83a83bc1da68f6c593d9e8a4558d98d9
Jerry <jerry.osorio@autoweb.com>
2021-09-10 20:50:28 -0600

    adding attributesExamples

---
+++ tests/attributesExamples.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+let browser = null;
+let page = null;
+
+describe('Attributes Examples', () => {
+	before(async () => {
+		browser = await puppeteer.launch({
+			headless: false,
+			devtools: false,
+		});
+		page = await browser.newPage();
+		await page.goto('https://the-internet.herokuapp.com/login');
+	});
+
+	it('Get element value', async () => {
+		const userNameInput = await page.waitForXPath('//input[@id="username"]');
+		await userNameInput.type('My User Name');
+		const inputValue = await page.evaluate(name => name.value, userNameInput);
+		console.log(inputValue);
+	});
+
+	it('Get element attribute', async () => {
+		const userNameInput = await page.waitForXPath('//input[@id="username"]');
+		await userNameInput.type('My User Name');
+		const inputType = await page.evaluate(name => name.type, userNameInput);
+		console.log(inputType);
+	});
+
+	after(async () => {
+		await browser.close();
+	});
+});
